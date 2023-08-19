4d62d49fe8073ac4d36ccd42f36b32c1191c7b00
Aaronphilip2003 <aaronphilip2003@gmail.com>
2023-08-19 10:46:01 +0530

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,52 +1,85 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
 import "./App.css";
-import { MainContainer, Sidebar, ChatContainer, ConversationList, Conversation, MessageList, ConversationHeader, Message } from "@chatscope/chat-ui-kit-react";
+import {
+  MainContainer,
+  Sidebar,
+  ChatContainer,
+  ConversationList,
+  Conversation,
+  MessageList,
+  ConversationHeader,
+  Message,
+} from "@chatscope/chat-ui-kit-react";
 import ChatComponent from "./components/ChatComponent";
 import TuringChatComponent from "./components/TuringChatComponent";
 import PsychChatComponent from "./components/PsychChatComponent";
 
-function App() {
-  const [   activeChat, setActiveChat] = useState("Norwegian Wood");
+function HomePage() {
+  return (
+    <div className="home-page">
+      <h1>Welcome to the Home Page</h1>
+      <Link to="/chats">
+        <button>Go to Chats</button>
+      </Link>
+    </div>
+  );
+}
+
+function ChatsPage() {
+  const [activeChat, setActiveChat] = useState("Norwegian Wood");
 
   const handleChatClick = (chatName) => {
     setActiveChat(chatName);
   };
 
   return (
-    <div className="App">
-      <MainContainer>
-        <Sidebar position="left">
-          <ConversationList>
-            <Conversation
-              name="Norwegian Wood"
-              onClick={() => handleChatClick("Norwegian Wood")}
-              active={activeChat === "Norwegian Wood"}
-            />
-            <Conversation
-              name="Turing"
-              onClick={() => handleChatClick("Turing")}
-              active={activeChat === "Turing"}
-            />
-            <Conversation
-              name="Psychology Of Money"
-              onClick={() => handleChatClick("Psych")}
-              active={activeChat === "Psych"}
-            />
-          </ConversationList>
-        </Sidebar>
-        <ChatContainer>
-          <ConversationHeader>
-            <ConversationHeader.Content userName={activeChat} />
-          </ConversationHeader>
-          <MessageList>
-            {activeChat === "Norwegian Wood" && <ChatComponent />}
-            {activeChat === "Turing" && <TuringChatComponent />}
-            {activeChat === "Psych" && <PsychChatComponent />}
-          </MessageList>
-          {/* Remove the MessageInput from here */}
-        </ChatContainer>
-      </MainContainer>
-    </div>
+    <MainContainer>
+      <Sidebar position="left">
+        <ConversationList>
+          <Conversation
+            name="Norwegian Wood"
+            onClick={() => handleChatClick("Norwegian Wood")}
+            active={activeChat === "Norwegian Wood"}
+          />
+          <Conversation
+            name="Turing"
+            onClick={() => handleChatClick("Turing")}
+            active={activeChat === "Turing"}
+          />
+          <Conversation
+            name="Psychology Of Money"
+            onClick={() => handleChatClick("Psych")}
+            active={activeChat === "Psych"}
+          />
+        </ConversationList>
+      </Sidebar>
+      <ChatContainer>
+        <ConversationHeader>
+          <ConversationHeader.Content userName={activeChat} />
+        </ConversationHeader>
+        <MessageList>
+          {activeChat === "Norwegian Wood" && <ChatComponent />}
+          {activeChat === "Turing" && <TuringChatComponent />}
+          {activeChat === "Psych" && <PsychChatComponent />}
+        </MessageList>
+        {/* Remove the MessageInput from here */}
+      </ChatContainer>
+    </MainContainer>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chats" element={<ChatsPage />} />
+          <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
