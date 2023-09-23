c0a60c76394692a7bcf3cb1be418e2f4b5328a57
Aaronphilip2003 <aaronphilip2003@gmail.com>
2023-09-23 12:15:26 +0530

    Added a few more chat components

---
+++ src/components/MOVChatComponent.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { MainContainer, ChatContainer, MessageList, MessageInput, Message } from "@chatscope/chat-ui-kit-react";
+import axios from "axios";
+import "./chat.css";
+
+const MOVChatComponent = () => {
+  const [messages, setMessages] = useState([]);
+
+  const API_ENDPOINT = "http://127.0.0.1:8000/query/mov/";
+
+  const sendMessage = (message) => {
+    const newMessage = {
+      message: message,
+      sender: "You",
+      sentTime: new Date().toLocaleString(),
+      direction: "outgoing",
+      position: "single",
+    };
+
+    setMessages((prevMessages) => [...prevMessages, newMessage]);
+
+    axios
+      .get(API_ENDPOINT, {
+        params: {
+          query: message,
+        },
+      })
+      .then((response) => {
+        const responseData = response.data;
+        const incomingMessages = responseData.answers.map((answer, index) => ({
+          message: answer,
+          sender: "Psych",
+          sentTime: new Date().toLocaleString(),
+          direction: "incoming",
+          position: index === responseData.answers.length - 1 ? "last" : "single",
+        }));
+        setMessages((prevMessages) => [...prevMessages, ...incomingMessages]);
+      })
+      .catch((error) => {
+        console.error("Error sending query:", error);
+      });
+  };
+
+  return (
+    <div className="chatPage-div">
+      <MainContainer>
+        <ChatContainer>
+          {/* No ConversationHeader */}
+          <MessageList>
+            {messages.map((msg, index) => (
+              <Message
+                key={index}
+                model={{
+                  message: msg.message,
+                  sender: msg.sender,
+                  sentTime: msg.sentTime,
+                  direction: msg.direction,
+                  position: msg.position,
+                }}
+              />
+            ))}
+          </MessageList>
+          {/* MessageInput for Psych */}
+          <MessageInput placeholder="Chat with MOV......" onSend={sendMessage} />
+        </ChatContainer>
+      </MainContainer>
+    </div>
+  );
+};
+
+export default MOVChatComponent;
